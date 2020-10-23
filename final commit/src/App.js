9b8157d8166e9c14c169ad10ed451d83c28f9eaa
Pranali <71920435+PranaliChhaya@users.noreply.github.com>
2020-10-23 06:22:08 -0500

    First commit
    first commit for redux project

---
+++ final commit/src/App.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import Api from "./components/Api";
+
+
+const App = () => {
+  return (
+    <Fragment>
+      <Api/>
+    </Fragment>
+  );
+};
+
+export default App;
+
+
+
+
+
+
+
